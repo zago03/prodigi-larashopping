f78474dac46b97cd019684fb57b1159ed838bbf7
Fabrizio Zagone <fabriziozagone03@gmail.com>
2023-03-20 17:42:44 +0100

    larashooping sew trolley

---
+++ resources/views/carts/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('head_title','Carrello')
+
+@section('content')
+
+<h4>Carrello</h4>
+
+@include('partials.navbar',["categories"=>$categoires])
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+@endsection
